3206098feaeb325eec791143e2b9e5ea31060754
hanjia2014 <hanj@datacom.co.nz>
2016-02-04 09:16:09 +1300

    add services

---
+++ Scripts/demoApp/interfaces/IRepositoryService.ts
@@ -0,0 +1,9 @@
+﻿module DemoApp.Interfaces {
+    import Models = DemoApp.Models;
+    export interface IRepositoryService {
+        Fetch: <T>() => Array<T>;
+        FetchPromise: <T>() => ng.IPromise<T>;
+        FetchCompany: (name: string) => Models.Company;
+        FetchEmployee: (name: string) => Models.Employee;
+    }
+}
