859ced0462f3ee2b811ed8f7f2b0dbabcf130992
hanjia2014 <hanj@datacom.co.nz>
2016-02-05 16:35:41 +1300

    add signalr but with errors

--- Scripts/demoApp/app.ts
+++ Scripts/demoApp/app.ts
@@ -1,3 +1,10 @@
 ﻿module DemoApp {
+    var demoAppHub = $.connection.demoAppHub;
+    $(function () {
+        $.connection.hub.logging = true;
+        $.connection.hub.start();
+    });
+
     var app = angular.module("demoApp", []);
+    app.value("demoAppHub", demoAppHub);
 }
