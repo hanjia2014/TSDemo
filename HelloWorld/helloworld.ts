2bc41c7073ad7ee29b0fb19d98ca99846c63dbfa
hanjia2014 <hanj@datacom.co.nz>
2016-02-17 09:00:51 +1300

    add simple example

---
+++ HelloWorld/helloworld.ts
@@ -0,0 +1,12 @@
+﻿module HelloWorld_Sample {
+    export class Greeter {
+        constructor(public greeting: string) { }
+        greet() {
+            return "<h1>" + this.greeting + "</h1>";
+        }
+    };
+}
+
+var greeter = new HelloWorld_Sample.Greeter("Hello, world!");
+
+document.body.innerHTML = greeter.greet();
