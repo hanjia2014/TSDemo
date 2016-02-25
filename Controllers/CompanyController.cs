2158e925e5411d2e799df39663629ac1e74f3319
hanjia2014 <hanj@datacom.co.nz>
2016-02-26 09:21:47 +1300

    change company name

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -10,7 +10,7 @@ namespace TSDemo.Controllers
         {
             var datacomEmployees = new List<Employee> { new Employee { Id = 1, Name = "John Doe 1", CompanyId = 1 }, new Employee { Id = 2, Name = "John Doe 2", CompanyId = 1 }, new Employee { Id = 3, Name = "John Doe 3", CompanyId = 1 } };
             var trademeEmployees = new List<Employee> { new Employee { Id = 1, Name = "John Doe 4", CompanyId = 2 }, new Employee { Id = 2, Name = "John Doe 5", CompanyId = 2 }, new Employee { Id = 3, Name = "John Doe 6", CompanyId = 2 } };
-            return new List<Company> { new Company { Id = 1, Name = "Datacom", Address = "Wellington", Employees = datacomEmployees }, new Company { Id = 2, Name = "Trade Me", Address = "Auckland", Employees = trademeEmployees } };
+            return new List<Company> { new Company { Id = 1, Name = "Company A", Address = "Wellington", Employees = datacomEmployees }, new Company { Id = 2, Name = "Company B", Address = "Auckland", Employees = trademeEmployees } };
         }
     }
 }
